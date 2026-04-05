bd9313c83ce51055b91905dd832e9a4c30027153
Wagner Bertholdo Burghausen <wagb2b2@gmail.com>
2026-04-04 23:47:09 -0300

    Add more constraints in class User

--- src/main/java/tech/ada/model/User.java
+++ src/main/java/tech/ada/model/User.java
@@ -6,11 +6,13 @@ import io.quarkus.security.jpa.Password;
 import io.quarkus.security.jpa.Roles;
 import io.quarkus.security.jpa.UserDefinition;
 import io.quarkus.security.jpa.Username;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 @Entity
 @Table(name = "users")
@@ -20,15 +22,21 @@ public class User extends PanacheEntity {
     @NotBlank(message = "Name must not be blank")
     @Username
     private String username;
+
     @NotBlank(message = "Email must not be blank")
+    @Column(nullable = false, unique = true)
     @Email
     private String email;
+
     @NotBlank(message = "Password must not be blank")
+    @Size(min = 8, message = "Password must have at least 8 characters")
     @Password
     private String password;
+
     @NotBlank(message = "Role must not be blank")
     @Roles
     private String role;
+
 
     public String getUsername() {
         return username;
