e3b9ada38deb1ee99b26be0c2bc09f0d8596d21d
Wagner Bertholdo Burghausen <wagb2b2@gmail.com>
2026-04-04 23:32:33 -0300

    Add class User and security/jwt extensions

---
+++ src/main/java/tech/ada/model/User.java
@@ -0,0 +1,67 @@
+package tech.ada.model;
+
+import io.quarkus.elytron.security.common.BcryptUtil;
+import io.quarkus.hibernate.orm.panache.PanacheEntity;
+import io.quarkus.security.jpa.Password;
+import io.quarkus.security.jpa.Roles;
+import io.quarkus.security.jpa.UserDefinition;
+import io.quarkus.security.jpa.Username;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+@Entity
+@Table(name = "users")
+@UserDefinition
+public class User extends PanacheEntity {
+
+    @NotBlank(message = "Name must not be blank")
+    @Username
+    private String username;
+    @NotBlank(message = "Email must not be blank")
+    @Email
+    private String email;
+    @NotBlank(message = "Password must not be blank")
+    @Password
+    private String password;
+    @NotBlank(message = "Role must not be blank")
+    @Roles
+    private String role;
+
+    public String getUsername() {
+        return username;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public String getRole() {
+        return role;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public static void add(@Valid String username, @Valid String email,
+                           @Valid String password, @Valid String role) {
+        User user = new User();
+        user.setUsername(username);
+        user.setEmail(email);
+        user.setPassword(BcryptUtil.bcryptHash(password));
+        user.setRole(role);
+        user.persist();
+    }
+}
