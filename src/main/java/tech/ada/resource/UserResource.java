3a822391d50144e4ac046b9181c906682b69c758
Wagner Bertholdo Burghausen <wagb2b2@gmail.com>
2026-04-12 18:11:39 -0300

    create addUser method in UserResource

--- src/main/java/tech/ada/resource/UserResource.java
+++ src/main/java/tech/ada/resource/UserResource.java
@@ -1,12 +1,41 @@
 package tech.ada.resource;
 
+import jakarta.validation.Valid;
 import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import tech.ada.dto.UserRequestDTO;
+import tech.ada.dto.UserResponseDTO;
+import tech.ada.model.User;
+import tech.ada.service.UserService;
+
+import java.net.URI;
 
 @Path("/users")
 @Consumes(MediaType.APPLICATION_JSON)
 @Produces(MediaType.APPLICATION_JSON)
 public class UserResource {
+    private final UserService service;
+
+    public UserResource(UserService service) {
+        this.service = service;
+    }
+
+    @POST
+    public Response addUser(@Valid UserRequestDTO dto) {
+        User newUser = service.addUser(dto);
+
+        UserResponseDTO payload = new UserResponseDTO(
+                newUser.getUsername(), newUser.getEmail(),
+                newUser.getRole()
+        );
+
+        return Response.status(201)
+                .header("Content-Type", "application/json")
+                .entity(payload)
+                .build();
+    }
 }
