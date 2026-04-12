424d737e24634fba34eedb19eda42f47070a2b10
Wagner Bertholdo Burghausen <wagb2b2@gmail.com>
2026-04-12 18:56:45 -0300

    rename class

--- src/main/java/tech/ada/security/JwtGenerator.java
+++ src/main/java/tech/ada/security/JwtGenerator.java
@@ -2,7 +2,7 @@ package tech.ada.security;
 
 import io.smallrye.jwt.build.Jwt;
 
-public class GenerateJwt {
+public class JwtGenerator {
     public String generateJws() {
         return Jwt.claims().sign();
     }
