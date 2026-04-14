3544be6dbe792b4dc2754c5e82f6f876a0e3edde
Wagner Bertholdo Burghausen <wagb2b2@gmail.com>
2026-04-13 21:03:30 -0300

    create UserSignInDTO

---
+++ src/main/java/tech/ada/dto/UserSignInDTO.java
@@ -0,0 +1,16 @@
+package tech.ada.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record UserSignInDTO (
+        @NotBlank(message = "Email must not be blank")
+        @Email
+        String email,
+
+        @NotBlank(message = "Password must not be blank")
+        @Size(min = 8, message = "Password must have at least 8 characters")
+        String password
+) {
+}
